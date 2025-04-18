aa0420d5bec57aba38533e22e6c8b8c258076009
Juliano Sill <sill.juliano@gmail.com>
2025-04-17 23:14:27 -0300

    feat(components): create `Input` component

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,65 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import type { LucideIcon } from 'lucide-react'
+
+import { cn } from '@/utils/class-name-merge'
+
+const inputVariants = cva(
+  'ring-offset-background focus-visible:ring-ring [&_svg]: h-10 w-full shrink-0 rounded-lg border bg-transparent px-3 text-sm shadow-xs transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default:
+          'border-border text-foreground placeholder:text-foreground-soft',
+        error: 'border-error text-error focus-visible:ring-error',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+)
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> &
+  VariantProps<typeof inputVariants> & {
+    icon?: LucideIcon
+  }
+
+export function Input({
+  icon,
+  className,
+  variant,
+  ...props
+}: Readonly<InputProps>) {
+  const Icon = icon
+
+  const iconColors = {
+    default: 'text-disabled',
+    error: 'text-error',
+  }
+
+  if (Icon) {
+    return (
+      <div
+        className={cn(
+          'relative flex w-full items-center [&>svg]:size-4.5',
+          props.disabled && '[&>svg]:opacity-50',
+        )}
+      >
+        <input
+          className={cn(inputVariants({ variant, className }), 'pl-10')}
+          {...props}
+        />
+        <Icon
+          className={cn(
+            'pointer-events-none absolute left-3 shrink-0 transition-colors',
+            iconColors[variant ?? 'default'],
+          )}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <input className={cn(inputVariants({ variant, className }))} {...props} />
+  )
+}
