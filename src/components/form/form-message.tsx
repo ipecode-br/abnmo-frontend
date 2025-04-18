e29fc749e9b0d4515384bb3edbbe8b90956764e8
Juliano Sill <sill.juliano@gmail.com>
2025-04-17 23:59:24 -0300

    feat(components): create `FormMessage`

---
+++ src/components/form/form-message.tsx
@@ -0,0 +1,47 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import { AlertCircleIcon } from 'lucide-react'
+
+import { cn } from '@/utils/class-name-merge'
+
+const formMessageVariants = cva('mt-1 flex items-center gap-1 text-xs', {
+  variants: {
+    variant: {
+      default: 'text-foreground-soft',
+      error: 'text-error',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+})
+
+interface FormMessageProps
+  extends React.HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof formMessageVariants> {
+  error?: boolean
+}
+
+export function FormMessage({
+  error,
+  variant,
+  className,
+  children,
+  ...props
+}: Readonly<FormMessageProps>) {
+  if (!children) return null
+
+  return (
+    <p
+      className={cn(
+        formMessageVariants({
+          variant: error ? 'error' : variant,
+          className,
+        }),
+      )}
+      {...props}
+    >
+      {error && <AlertCircleIcon className='size-4' />}
+      {children}
+    </p>
+  )
+}
