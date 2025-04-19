eb04f0b743ae570a2317aaae46b1383c0129db4b
Juliano Sill <sill.juliano@gmail.com>
2025-04-18 22:24:51 -0300

    chore(auth-card): add icon image

--- src/components/auth/auth-card.tsx
+++ src/components/auth/auth-card.tsx
@@ -1,3 +1,5 @@
+import icon from '@images/auth/icon.svg'
+import Image from 'next/image'
 import type { ReactNode } from 'react'
 
 interface AuthCardProps {
@@ -13,7 +15,8 @@ export function AuthCard({
 }: Readonly<AuthCardProps>) {
   return (
     <div className='bg-background flex w-full max-w-md flex-col items-center gap-6 rounded-3xl p-8 shadow-xl/5'>
-      <header className='space-y-1 text-center'>
+      <header className='flex flex-col items-center gap-1 text-center'>
+        <Image src={icon} alt='' className='size-20' />
         <h1 className='text-2xl font-medium'>{title}</h1>
         <p className='text-foreground-soft'>{description}</p>
       </header>
