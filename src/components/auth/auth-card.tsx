0ada38e3324c0e50542eb82ee206fcc21337d530
Juliano Sill <sill.juliano@gmail.com>
2025-04-20 11:29:11 -0300

    chore(auth-card): make image dynamically rendered

--- src/components/auth/auth-card.tsx
+++ src/components/auth/auth-card.tsx
@@ -1,16 +1,18 @@
-import icon from '@images/auth/icon.svg'
+import type { StaticImport } from 'next/dist/shared/lib/get-img-props'
 import Image from 'next/image'
 import type { ReactNode } from 'react'
 
 import { Divider } from '@/components/ui/divider'
 
 interface AuthCardProps {
+  image: string | StaticImport
   title: string
   description: string
   children: ReactNode
 }
 
 export function AuthCard({
+  image,
   title,
   description,
   children,
@@ -18,7 +20,7 @@ export function AuthCard({
   return (
     <div className='bg-background flex w-full max-w-md flex-col items-center gap-6 rounded-3xl p-8 shadow-xl/5'>
       <header className='flex flex-col items-center gap-1 text-center'>
-        <Image src={icon} alt='' className='size-20' />
+        {image && <Image src={image} alt='' className='size-20' />}
         <h1 className='text-2xl font-medium'>{title}</h1>
         <p className='text-foreground-soft'>{description}</p>
       </header>
