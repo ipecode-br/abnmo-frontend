899acd8f06aa513d07f46cf41463633a1d0fdf3a
Juliano Sill <sill.juliano@gmail.com>
2025-04-20 11:16:58 -0300

    fix(auth): update layout background color

--- src/app/conta/layout.tsx
+++ src/app/conta/layout.tsx
@@ -2,7 +2,7 @@ export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className='flex min-h-svh w-full items-center justify-center bg-[url("/images/background/auth-bg.svg")] bg-center bg-no-repeat p-4'>
+    <div className='bg-background-soft flex min-h-svh w-full items-center justify-center bg-[url("/images/background/auth-bg.svg")] bg-center bg-no-repeat p-4'>
       {children}{' '}
     </div>
   )
