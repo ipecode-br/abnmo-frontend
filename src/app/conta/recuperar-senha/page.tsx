5353c3c2c0c9e9679187810f8881196456b8fc6e
Juliano Sill <sill.juliano@gmail.com>
2025-04-20 13:52:21 -0300

    feat(auth): create `RecoverPasswordPage`

---
+++ src/app/conta/recuperar-senha/page.tsx
@@ -0,0 +1,37 @@
+import image from '@images/auth/recover.svg'
+import type { Metadata } from 'next'
+
+import { AuthCard } from '@/components/auth/auth-card'
+import { Divider } from '@/components/ui/divider'
+import { NavLink } from '@/components/ui/nav-link'
+import { ROUTES } from '@/constants/routes'
+
+import { RecoverForm } from './recover-in-form'
+
+export const metadata: Metadata = {
+  title: 'Recuperar senha',
+}
+
+export default function RecoverPasswordPage() {
+  return (
+    <AuthCard
+      image={image}
+      title='Recuperar senha'
+      description='Insira seu e-mail para recuperar sua senha'
+    >
+      <RecoverForm />
+
+      <Divider />
+
+      <p className='text-foreground-soft text-center text-sm'>
+        Mudou de ideia?{' '}
+        <NavLink
+          href={ROUTES.auth.signIn}
+          className='text-foreground font-medium whitespace-nowrap'
+        >
+          Acesse sua conta
+        </NavLink>
+      </p>
+    </AuthCard>
+  )
+}
