4a7accc9039b2845340edfdf709490a359026f2c
Juliano Sill <sill.juliano@gmail.com>
2025-04-19 23:41:51 -0300

    feat(components): create `Divider`

---
+++ src/components/ui/divider.tsx
@@ -0,0 +1,40 @@
+import { cn } from '@/utils/class-name-merge'
+
+interface DividerProps {
+  flexItem?: boolean
+  height?: string
+  orientation?: 'horizontal' | 'vertical'
+  text?: string
+  width?: string
+}
+
+export function Divider({
+  flexItem,
+  height,
+  orientation = 'horizontal',
+  text,
+  width = 'w-full',
+}: Readonly<DividerProps>) {
+  if (orientation === 'vertical') {
+    return (
+      <div
+        className={cn('bg-border w-px', flexItem && 'self-stretch', height)}
+      />
+    )
+  }
+
+  return (
+    <>
+      {text ? (
+        <div className={cn('relative flex items-center justify-center', width)}>
+          <p className='bg-background text-foreground-soft z-10 px-3 text-sm'>
+            {text}
+          </p>
+          <div className='bg-border absolute h-px w-full' />
+        </div>
+      ) : (
+        <div className={cn('bg-border h-px', width)} />
+      )}
+    </>
+  )
+}
