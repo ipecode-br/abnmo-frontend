a469d3111bb39764db45d84c4e9ed3a1e2305ff5
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-04-19 22:27:12 -0300

    test: create test for label component

--- tests/components/ui/label.test.tsx
+++ tests/components/ui/label.test.tsx
@@ -1,7 +1,33 @@
 import '@testing-library/jest-dom'
 
-// TODO: implement tests
+import { render, screen } from '@testing-library/react'
+
+import { Label } from '@/components/ui/label'
 
 describe('Label', () => {
-  it('should render a default Label', () => {})
+  it('should render a default Label', () => {
+    const text = ' Label name'
+
+    render(<Label htmlFor='name'>{text}</Label>)
+
+    const labelElement = screen.getByText(text)
+
+    expect(labelElement).toBeInTheDocument()
+    expect(labelElement).toHaveClass('text-sm')
+    expect(labelElement).toHaveClass('font-medium')
+    expect(labelElement).toHaveAttribute('for', 'name')
+  })
+
+  it('should render Label with custom class', () => {
+    render(
+      <Label htmlFor='another-name' className='text-blue-500 italic'>
+        Label text
+      </Label>,
+    )
+
+    const labelElement = screen.getByText('Label text')
+
+    expect(labelElement).toHaveClass('text-blue-500')
+    expect(labelElement).toHaveClass('italic')
+  })
 })
