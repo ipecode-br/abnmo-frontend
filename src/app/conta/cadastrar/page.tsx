a03c1b5ef174b7916f393b860f2504e8b1e8ff8d
Juliano Sill <sill.juliano@gmail.com>
2025-04-20 17:58:20 -0300

    feat(auth): create `SignUpPage`

---
+++ src/app/conta/cadastrar/page.tsx
@@ -0,0 +1,37 @@
+import image from '@images/auth/sign-in.svg'
+import type { Metadata } from 'next'
+
+import { AuthCard } from '@/components/auth/auth-card'
+import { Divider } from '@/components/ui/divider'
+import { NavLink } from '@/components/ui/nav-link'
+import { ROUTES } from '@/constants/routes'
+
+import { SignUpForm } from './sign-up-form'
+
+export const metadata: Metadata = {
+  title: 'Cadastrar conta',
+}
+
+export default function SignUpPage() {
+  return (
+    <AuthCard
+      image={image}
+      title='Cadastrar conta'
+      description='Insira seus dados para criar sua conta'
+    >
+      <SignUpForm />
+
+      <Divider text='ou' />
+
+      <p className='text-foreground-soft text-center text-sm'>
+        Já tem uma conta?{' '}
+        <NavLink
+          href={ROUTES.auth.signIn}
+          className='text-foreground font-medium whitespace-nowrap'
+        >
+          Acesse sua conta
+        </NavLink>
+      </p>
+    </AuthCard>
+  )
+}
