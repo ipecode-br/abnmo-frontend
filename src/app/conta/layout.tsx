15f9626fd2698a3f68755c478c787aff92b4e9f4
Juliano Sill <sill.juliano@gmail.com>
2025-04-20 21:50:03 -0300

    chore(auth): update vertical padding on `layout.tsx`

--- src/app/conta/layout.tsx
+++ src/app/conta/layout.tsx
@@ -2,7 +2,7 @@ export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className='bg-background-soft flex min-h-svh w-full items-center justify-center bg-[url("/images/background/auth-bg.svg")] bg-center bg-no-repeat p-4'>
+    <div className='bg-background-soft flex min-h-svh w-full items-center justify-center bg-[url("/images/background/auth-bg.svg")] bg-center bg-no-repeat px-4 py-8'>
       {children}{' '}
     </div>
   )
