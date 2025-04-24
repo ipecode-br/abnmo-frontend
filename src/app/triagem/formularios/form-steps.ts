11c3b8f139b40b6c4e3743e6544151a7a30071fd
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-04-24 18:03:12 -0300

    feat(screening-progress): progress component with active route detection

---
+++ src/app/triagem/formularios/form-steps.ts
@@ -0,0 +1,20 @@
+export const steps = [
+  {
+    label: 'Seus dados',
+    indicator: '1',
+    info: 'Preencha os campos para prosseguir para a próxima etapa.',
+    path: '/triagem/formularios/seus-dados',
+  },
+  {
+    label: 'Laudo Médico',
+    indicator: '2',
+    info: 'Preencha os campos e anexe os documentos necessários.',
+    path: '/triagem/formularios/laudo-medico',
+  },
+  {
+    label: 'Rede de apoio',
+    indicator: '3',
+    info: 'Informe os dados de contato necessários.',
+    path: '/triagem/formularios/rede-de-apoio',
+  },
+]
--- src/app/triagem/formularios/screening-progress.tsx
+++ src/app/triagem/formularios/screening-progress.tsx
@@ -1,3 +1,39 @@
+'use client'
+import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+import { steps } from './form-steps'
+
 export function ScreeningProgress() {
-  return <aside>Progresso</aside>
+  const [activeStep, setActiveStep] = useState<number>()
+  const pathname = usePathname()
+
+  useEffect(() => {
+    const activeIndex = steps.findIndex((step) => step.path === pathname)
+    setActiveStep(activeIndex)
+  }, [pathname])
+
+  return (
+    <aside className='w-72'>
+      <h1 className='text-soft-400 pb-8 text-xs'>TRIAGEM</h1>
+
+      {steps.map((step, idx) => (
+        <div key={idx}>
+          <div
+            className={`inline-flex items-center gap-4 text-base font-medium ${activeStep === idx ? 'font-semibold' : 'text-neutras-500'}`}
+          >
+            <span
+              className={`flex size-6 items-center justify-center rounded-full border p-1 text-sm ${activeStep === idx ? 'bg-primaria-700 border-priamria-400 text-white' : 'text-sub-500 border-neutras-100'}`}
+            >
+              {step.indicator}
+            </span>
+            {step.label}
+          </div>
+          <p className='text-neutras-500 mt-2 mb-8 pl-10 text-sm'>
+            {step.info}
+          </p>
+        </div>
+      ))}
+    </aside>
+  )
 }
