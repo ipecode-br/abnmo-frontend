69a189bad2f968fa88e3f78a7e57e6496c92b513
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-04-24 12:49:32 -0300

    test: add test for NavButton component

--- tests/components/ui/nav-button.test.tsx
+++ tests/components/ui/nav-button.test.tsx
@@ -1,11 +1,108 @@
 import '@testing-library/jest-dom'
 
-// TODO: implement tests
+import { render, screen } from '@testing-library/react'
+
+import { NavButton } from '@/components/ui/nav-button'
 
 describe('NavButton', () => {
-  it('should render a default NavButton', () => {})
-  it('should render a fancy NavButton', () => {})
-  it('should render an outlined NavButton', () => {})
-  it('should render a small NavButton', () => {})
-  it('should render an icon NavButton with icon size', () => {})
+  it('should render a default NavButton', () => {
+    const buttonText = 'Home'
+    render(<NavButton href='/'>{buttonText}</NavButton>)
+
+    const fancyButtonElement = screen.getByText(buttonText)
+    expect(fancyButtonElement).toBeInTheDocument()
+
+    expect(fancyButtonElement).toHaveAttribute('href', '/')
+
+    expect(fancyButtonElement).toHaveClass('bg-primary')
+    expect(fancyButtonElement).toHaveClass('text-primary-foreground')
+    expect(fancyButtonElement).toHaveClass('hover:bg-primary/80')
+    expect(fancyButtonElement).toHaveClass('shadow-xs')
+    expect(fancyButtonElement).toHaveClass('h-10')
+    expect(fancyButtonElement).toHaveClass('px-4')
+  })
+  it('should render a fancy NavButton', () => {
+    const buttonText = 'Fancy'
+    render(
+      <NavButton href='/' variant='fancy'>
+        {buttonText}
+      </NavButton>,
+    )
+
+    const fancyButtonElement = screen.getByText(buttonText)
+    expect(fancyButtonElement).toBeInTheDocument()
+
+    expect(fancyButtonElement).toHaveAttribute('href', '/')
+
+    expect(fancyButtonElement).toHaveClass('bg-primary')
+    expect(fancyButtonElement).toHaveClass('text-primary-foreground')
+    expect(fancyButtonElement).toHaveClass('hover:bg-primary/80')
+    expect(fancyButtonElement).toHaveClass('inset-shadow-md')
+    expect(fancyButtonElement).toHaveClass('shadow-xs')
+  })
+  it('should render an outlined NavButton', () => {
+    const buttonText = 'Outiline'
+    render(
+      <NavButton href='/' variant='outline'>
+        {buttonText}
+      </NavButton>,
+    )
+
+    const outlinedButtonElement = screen.getByText(buttonText)
+    expect(outlinedButtonElement).toBeInTheDocument()
+
+    expect(outlinedButtonElement).toHaveAttribute('href', '/')
+
+    expect(outlinedButtonElement).toHaveClass('border-border')
+    expect(outlinedButtonElement).toHaveClass('hover:bg-accent')
+    expect(outlinedButtonElement).toHaveClass('border')
+    expect(outlinedButtonElement).toHaveClass('bg-transparent')
+  })
+  it('should render a small NavButton', () => {
+    const buttonText = 'Small'
+    render(
+      <NavButton href='/' size='sm'>
+        {buttonText}
+      </NavButton>,
+    )
+
+    const smallButtonElement = screen.getByText(buttonText)
+    expect(smallButtonElement).toBeInTheDocument()
+
+    expect(smallButtonElement).toHaveAttribute('href', '/')
+
+    expect(smallButtonElement).toHaveClass('h-9')
+    expect(smallButtonElement).toHaveClass('rounded-md')
+    expect(smallButtonElement).toHaveClass('px-3')
+  })
+  it('should render an icon NavButton with icon size', () => {
+    const buttonText = 'icon'
+    render(
+      <NavButton href='/' size='icon'>
+        {buttonText}
+      </NavButton>,
+    )
+
+    const iconButtonElement = screen.getByText(buttonText)
+    expect(iconButtonElement).toBeInTheDocument()
+
+    expect(iconButtonElement).toHaveAttribute('href', '/')
+
+    expect(iconButtonElement).toHaveClass('size-10')
+  })
+  it('should render NavButton with custom class', () => {
+    const buttonText = 'custom'
+    render(
+      <NavButton href='/' className='text-xl'>
+        {buttonText}
+      </NavButton>,
+    )
+
+    const customizedButtonElement = screen.getByText(buttonText)
+    expect(customizedButtonElement).toBeInTheDocument()
+
+    expect(customizedButtonElement).toHaveAttribute('href', '/')
+
+    expect(customizedButtonElement).toHaveClass('text-xl')
+  })
 })
