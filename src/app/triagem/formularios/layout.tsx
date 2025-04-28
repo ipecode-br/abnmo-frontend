052d8893387583195e7d8cfc2da6d3e769b29753
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-04-28 14:56:10 -0300

    style: removed color variables and removed the padding that had been done

--- src/app/triagem/formularios/layout.tsx
+++ src/app/triagem/formularios/layout.tsx
@@ -4,7 +4,7 @@ export default function ScreeningFormsLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <main className='container mx-auto grid grid-cols-[20rem_1fr] gap-16 px-16 pt-16'>
+    <main className='container mx-auto grid grid-cols-[20rem_1fr] gap-16 px-8'>
       <ScreeningProgress />
       <main>{children}</main>
     </main>
