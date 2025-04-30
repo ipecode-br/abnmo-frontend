87cc3c0e0b99ad8af8564895bbfad2ac05a18e03
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-04-30 20:07:03 -0300

    chore(constants): list of genres

---
+++ src/constants/genres.ts
@@ -0,0 +1,6 @@
+export const GENRES = [
+  { label: 'Feminino', value: 'feminino' },
+  { label: 'Masculino', value: 'masculino' },
+  { label: 'Prefiro não dizer', value: 'na' },
+  { label: 'Outro', value: 'outro' },
+]
