6c2a4fec3cb76b7db10c3c537bd61872f446e28c
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-05-04 10:22:39 -0300

    refactor(constants): renamed genres.ts to genders.ts and putting the values ​​in English

--- src/constants/genres.ts
+++
@@ -1,6 +0,0 @@
-export const GENRES = [
-  { label: 'Feminino', value: 'feminino' },
-  { label: 'Masculino', value: 'masculino' },
-  { label: 'Prefiro não dizer', value: 'na' },
-  { label: 'Outro', value: 'outro' },
-]
