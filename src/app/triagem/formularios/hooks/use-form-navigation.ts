2ae2c03c39b1716828e9c33a0d9b56e23acf7229
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-05-04 10:44:44 -0300

    feat(screening-your-data): validation created before returning data saved in local storage

--- src/app/triagem/formularios/hooks/use-form-navigation.ts
+++ src/app/triagem/formularios/hooks/use-form-navigation.ts
@@ -1,11 +1,17 @@
 import { useRouter } from 'next/navigation'
+import { ZodSchema } from 'zod'
 
 import { getStorageItem, setStorageItem } from '@/helpers/local-storage'
 
 export const useFormNavigation = (storageKey: string) => {
   const router = useRouter()
 
-  const getStored = getStorageItem(storageKey) ?? {}
+  const getStored = <T>(schema: ZodSchema<T>) => {
+    const storedData = getStorageItem(storageKey)
+    const parsedValue = schema.safeParse(storedData)
+
+    return parsedValue.success ? parsedValue.data : {}
+  }
 
   const goPage = ({
     data,
