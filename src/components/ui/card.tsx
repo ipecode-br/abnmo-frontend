b34bba760192dfce0ec9b5b6cd99a39e677b3cf2
Juliano Sill <sill.juliano@gmail.com>
2025-06-01 23:13:13 -0300

    chore(card): add `bg-card` class

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,7 +7,7 @@ type CardProps = HTMLAttributes<HTMLDivElement>
 export function Card({ className, ...props }: Readonly<CardProps>) {
   return (
     <div
-      className={cn('rounded-2xl border p-4 shadow-xs', className)}
+      className={cn('bg-card rounded-2xl border p-4 shadow-xs', className)}
       {...props}
     />
   )
