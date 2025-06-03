3abb3275fe5ec104dd02f89c5d0beb2d4f82c697
Juliano Sill <sill.juliano@gmail.com>
2025-06-02 22:43:48 -0300

    fix(css): remove global border color to avoid overriding styles

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,7 +7,10 @@ type CardProps = HTMLAttributes<HTMLDivElement>
 export function Card({ className, ...props }: Readonly<CardProps>) {
   return (
     <div
-      className={cn('bg-card rounded-2xl border p-4 shadow-xs', className)}
+      className={cn(
+        'bg-card border-border rounded-2xl border p-4 shadow-xs',
+        className,
+      )}
       {...props}
     />
   )
