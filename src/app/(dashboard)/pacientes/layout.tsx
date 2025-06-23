de9ba200e89e83fac4019b699365a90d1b32db89
Juliano Sill <sill.juliano@gmail.com>
2025-06-23 00:02:59 -0300

    chore(dashboard.patients): add tab buttons

--- src/app/(dashboard)/pacientes/layout.tsx
+++ src/app/(dashboard)/pacientes/layout.tsx
@@ -1,13 +1,21 @@
 import { DashboardTabButtons } from '@/components/dashboard/tab-buttons'
+import { ROUTES } from '@/constants/routes'
 
 import { DashboardContainer } from '../container'
 
 export default function PatientsLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const tabButtons = [
+    {
+      title: 'Lista de pacientes',
+      path: ROUTES.dashboard.patients.main,
+    },
+  ]
+
   return (
     <div className='flex flex-1 flex-col'>
-      <DashboardTabButtons />
+      <DashboardTabButtons buttons={tabButtons} />
       <DashboardContainer className='flex flex-col gap-6'>
         {children}
       </DashboardContainer>
