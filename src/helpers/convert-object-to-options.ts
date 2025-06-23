7e5bcd06e2adfbe04d9b953f491f594ae1cb3542
Juliano Sill <sill.juliano@gmail.com>
2025-06-22 21:01:17 -0300

    chore(helpers): rename helper to `convertObjectToOptions`

--- src/helpers/convert-object-to-options.ts
+++ src/helpers/convert-object-to-options.ts
@@ -1,4 +1,4 @@
-export function convertEnumToOptions(nativeEnum: Record<string, string>) {
+export function convertObjectToOptions(nativeEnum: Record<string, string>) {
   const options = Object.entries(nativeEnum).map(([key, value]) => ({
     value: key,
     label: value,
