b2298bb2e39f57ebfc0b1b7728b397f1df2e5bb3
Juliano Sill <sill.juliano@gmail.com>
2025-07-20 23:17:18 -0300

    chore(components): create `DialogContainer` and update `DialogContent`

--- src/components/dialog/content.tsx
+++ src/components/dialog/content.tsx
@@ -1,37 +1,8 @@
-'use client'
-
-import * as DialogPrimitive from '@radix-ui/react-dialog'
-import { X } from 'lucide-react'
-
 import { cn } from '@/utils/class-name-merge'
-
-import { DialogOverlay } from './overlay'
 
 export function DialogContent({
   className,
-  children,
   ...props
-}: Readonly<React.ComponentProps<typeof DialogPrimitive.Content>>) {
-  return (
-    <DialogPrimitive.Portal>
-      <DialogOverlay className='dialog-overlay' />
-      <DialogPrimitive.Content
-        className={cn(
-          'fixed top-1/2 left-1/2 z-50 w-full max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white p-6 shadow-lg',
-          className,
-        )}
-        {...props}
-      >
-        <DialogPrimitive.Close
-          className={cn(
-            'ring-offset-background absolute top-4 right-4 rounded-sm opacity-70 transition-opacity',
-            'hover:opacity-100',
-          )}
-        >
-          <X className='h-4 w-4' />
-        </DialogPrimitive.Close>
-        {children}
-      </DialogPrimitive.Content>
-    </DialogPrimitive.Portal>
-  )
+}: Readonly<React.ComponentProps<'div'>>) {
+  return <div className={cn('p-5', className)} {...props} />
 }
