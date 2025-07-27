2c50234c9ff90c66f528fae89d8422b0b0096d85
Juliano Sill <sill.juliano@gmail.com>
2025-07-27 16:21:30 -0300

    chore(components): add border to `Avatar` only when src is not provided

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -12,7 +12,8 @@ export function Avatar({ src, className, ...props }: Readonly<AvatarProps>) {
   return (
     <div
       className={cn(
-        'border-border text-foreground-soft bg-background flex size-10 shrink-0 items-center justify-center overflow-hidden rounded-full border [&_svg]:size-5',
+        'text-foreground-soft bg-background flex size-10 shrink-0 items-center justify-center overflow-hidden rounded-full [&_svg]:size-5',
+        !src && 'border-border border',
         className,
       )}
       {...props}
