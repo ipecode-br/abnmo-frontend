e28aa727ca8359f84c936b089e633b3ed1724467
Carol Silveira <carolinesv.dev@gmail.com>
2025-08-05 05:02:22 -0300

    feat: add overview data display

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,38 +1,11 @@
-'use client'
+import type { Metadata } from 'next'
 
-import { DashboardContainer } from '@/components/dashboard/container'
-import { DashboardOverviewCard } from '@/components/dashboard/overview/card'
-import DashboardOverview from '@/components/dashboard/overview/dashboard-overview'
+import Overview from './visao/page'
 
-import { PATIENTS_MOCKS } from '../../utils/mock/patients'
+export const metadata: Metadata = {
+  title: 'Visão Geral',
+}
 
-export default function SupportPage() {
-  const totalPatients = PATIENTS_MOCKS.length
-  const activePatients = PATIENTS_MOCKS.filter(
-    (patient) => patient.status === 'active',
-  ).length
-  const inactivePatients = PATIENTS_MOCKS.filter(
-    (patient) => patient.status === 'inactive',
-  ).length
-
-  return (
-    <DashboardContainer className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-      <DashboardOverview />
-      <DashboardOverviewCard
-        title='Total de Pacientes'
-        value={totalPatients}
-        variant='default'
-      />
-      <DashboardOverviewCard
-        title='Total de Pacientes Ativos'
-        value={activePatients}
-        variant='active'
-      />
-      <DashboardOverviewCard
-        title='Total de Pacientes Inativos'
-        value={inactivePatients}
-        variant='inactive'
-      />
-    </DashboardContainer>
-  )
+export default function VisaoPage() {
+  return <Overview />
 }
---
+++ src/app/(dashboard)/visao/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { DashboardContainer } from '@/components/dashboard/container'
+import { DashboardOverviewCard } from '@/components/dashboard/overview/card'
+import DashboardOverview from '@/components/dashboard/overview/dashboard-overview'
+
+import { PATIENTS_MOCKS } from '../../../utils/mock/patients'
+
+export default function Overview() {
+  const totalPatients = PATIENTS_MOCKS.length
+  const activePatients = PATIENTS_MOCKS.filter(
+    (patient) => patient.status === 'active',
+  ).length
+  const inactivePatients = PATIENTS_MOCKS.filter(
+    (patient) => patient.status === 'inactive',
+  ).length
+
+  return (
+    <DashboardContainer className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+      <DashboardOverview />
+      <DashboardOverviewCard
+        title='Total de Pacientes'
+        value={totalPatients}
+        variant='default'
+      />
+      <DashboardOverviewCard
+        title='Total de Pacientes Ativos'
+        value={activePatients}
+        variant='active'
+      />
+      <DashboardOverviewCard
+        title='Total de Pacientes Inativos'
+        value={inactivePatients}
+        variant='inactive'
+      />
+    </DashboardContainer>
+  )
+}
