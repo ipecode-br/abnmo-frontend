a739f05ede18ecb350f0a9e8145163c9f0ca858e
Juliano Sill <sill.juliano@gmail.com>
2025-08-06 22:29:39 -0300

    chore(components): remove type from chart tooltip

--- src/components/charts/custom-tooltip.tsx
+++ src/components/charts/custom-tooltip.tsx
@@ -1,19 +1,5 @@
-import type { TooltipProps } from 'recharts'
-import type {
-  NameType,
-  ValueType,
-} from 'recharts/types/component/DefaultTooltipContent'
-
-interface CustomTooltipProps extends TooltipProps<ValueType, NameType> {
-  coordinate?: { x: number; y: number }
-  payload: { value: number }[]
-}
-
-export function CustomTooltip({
-  active,
-  payload,
-  coordinate,
-}: CustomTooltipProps) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function CustomTooltip({ active, payload, coordinate }: any) {
   if (!active || !payload || !payload.length || !coordinate) return null
 
   const { x, y } = coordinate
