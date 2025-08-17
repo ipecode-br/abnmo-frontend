db3ab9f763f27ffac6d5eaf5fb64990c9c60cfb1
Juliano Sill <sill.juliano@gmail.com>
2025-08-17 17:55:25 -0300

    fix(config): undo unnecessary changes in `env.ts`

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,11 +1,17 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  // APIs
   NEXT_PUBLIC_API_URL: z.string().url(),
-  NEXT_PUBLIC_API_IBGE: z.string(),
+  NEXT_PUBLIC_API_IBGE: z.string().url(),
+
+  // Environment
+  NODE_ENV: z
+    .enum(['production', 'test', 'development'])
+    .default('development'),
 })
 
 export const env = envSchema.parse({
   NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  NEXT_PUBLIC_API_IBGE: process.env.NEXT_PUBLIC_API_IBGE || '',
+  NEXT_PUBLIC_API_IBGE: process.env.NEXT_PUBLIC_API_IBGE,
 })
