981d6e60643110a3974d801040e9729be25c9caf
Juliano Sill <sill.juliano@gmail.com>
2025-08-17 17:53:50 -0300

    fix(middleware): undo unnecessary changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,6 +23,14 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - images (Public image route)
+     * - 404 (404 error page)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - icon.png (favicon file)
+     */
     '/((?!images|404|_next/static|_next/image|icon.png|sitemap.xml|robots.txt|favicon.ico).*)',
   ],
 }
