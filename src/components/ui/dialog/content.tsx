19572a06583c16cb8a6a00a3f269fbba9723abc9
Juliano Sill <sill.juliano@gmail.com>
2025-11-27 00:32:24 -0300

    feat(components): migrate dialog from radix to base-ui

--- src/components/ui/dialog/content.tsx
+++
@@ -1,8 +0,0 @@
-import { cn } from '@/utils/class-name-merge'
-
-export function DialogContent({
-  className,
-  ...props
-}: Readonly<React.ComponentProps<'div'>>) {
-  return <div className={cn('p-5', className)} {...props} />
-}
