0efb595c1417c6ea4bdb74ca0ce37bf57476ab3d
Juliano Sill <sill.juliano@gmail.com>
2026-01-25 01:02:17 -0300

    feat(dashboard): add cancel appointment modal

---
+++ src/modules/appointments/cancel-appointment-modal.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import { CircleXIcon } from 'lucide-react'
+import { useTransition } from 'react'
+import { toast } from 'sonner'
+
+import { revalidateCache } from '@/actions/cache'
+import { Button } from '@/components/ui/button'
+import {
+  DialogClose,
+  DialogContainer,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogIcon,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import { NEXT_CACHE_TAGS, QUERY_CACHE_KEYS } from '@/constants/cache'
+import { api } from '@/lib/api'
+import { queryClient } from '@/lib/tanstack-query'
+import type { Appointment } from '@/types/appointments'
+import { formatDate } from '@/utils/formatters/format-date'
+
+interface CancelAppointmentModalProps {
+  appointment: Appointment
+  onClose: () => void
+}
+
+export function CancelAppointmentModal({
+  appointment,
+  onClose,
+}: Readonly<CancelAppointmentModalProps>) {
+  const [isPending, startTransition] = useTransition()
+
+  async function cancelAppointment() {
+    startTransition(async () => {
+      const response = await api(`/appointments/${appointment.id}/cancel`, {
+        method: 'PATCH',
+      })
+
+      if (!response.success) {
+        toast.error(response.message)
+        return
+      }
+
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_CACHE_KEYS.appointments.list],
+      })
+      revalidateCache(NEXT_CACHE_TAGS.patient(appointment.patient_id))
+      toast.success(response.message)
+      onClose()
+    })
+  }
+
+  return (
+    <DialogContainer>
+      <DialogHeader icon={<DialogIcon icon={CircleXIcon} />}>
+        <DialogTitle>Cancelar atendimento</DialogTitle>
+      </DialogHeader>
+
+      <DialogContent>
+        Você tem certeza que deseja cancelar o atendimento de{' '}
+        <span className='font-medium'>{appointment.patient.name}</span>, dia{' '}
+        <span className='font-medium'>{formatDate(appointment.date)}</span>?
+      </DialogContent>
+
+      <DialogFooter>
+        <Button
+          className='flex-1'
+          loading={isPending}
+          variant='destructive'
+          onClick={cancelAppointment}
+        >
+          Confirmar cancelamento
+        </Button>
+        <DialogClose className='flex-1' disabled={isPending}>
+          Cancelar
+        </DialogClose>
+      </DialogFooter>
+    </DialogContainer>
+  )
+}
--- src/modules/dashboard/appointments-card-actions.tsx
+++ src/modules/dashboard/appointments-card-actions.tsx
@@ -8,65 +8,85 @@ import {
   XCircleIcon,
 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
+import { Dialog } from '@/components/ui/dialog'
 import { Divider } from '@/components/ui/divider'
 import { Menu, MenuContent, MenuItem, MenuTrigger } from '@/components/ui/menu'
 import { ROUTES } from '@/constants/routes'
 import type { Appointment } from '@/types/appointments'
 
+import { CancelAppointmentModal } from '../appointments/cancel-appointment-modal'
+
 interface DashboardAppointmentsCardActionsProps {
   appointment: Appointment
 }
 
-// TODO: create cancel appointment modal
+// TODO: add edit appointment functionality
 
 export function DashboardAppointmentsCardActions({
   appointment,
 }: Readonly<DashboardAppointmentsCardActionsProps>) {
+  const [cancelModalOpen, setCancelModalOpen] = useState(false)
+
   const router = useRouter()
 
   return (
-    <Menu>
-      <MenuTrigger size='icon' variant='ghost' className='size-8'>
-        <EllipsisIcon />
-        <span className='sr-only'>Ações</span>
-      </MenuTrigger>
+    <>
+      <Menu>
+        <MenuTrigger size='icon' variant='ghost' className='size-8'>
+          <EllipsisIcon />
+          <span className='sr-only'>Ações</span>
+        </MenuTrigger>
 
-      <MenuContent align='end'>
-        <MenuItem
-          onClick={() => router.push(ROUTES.dashboard.appointments.list)}
-        >
-          <ClipboardPenIcon />
-          Editar
-        </MenuItem>
-        <MenuItem
-          onClick={() =>
-            router.push(
-              ROUTES.dashboard.patients.details.info(appointment.patient_id),
-            )
-          }
-        >
-          <ClipboardListIcon />
-          Informações do paciente
-        </MenuItem>
-        <MenuItem
-          onClick={() =>
-            router.push(
-              ROUTES.dashboard.patients.details.history(appointment.patient_id),
-            )
-          }
-        >
-          <ClipboardCheckIcon />
-          Histórico do paciente
-        </MenuItem>
+        <MenuContent align='end'>
+          <MenuItem>
+            <ClipboardPenIcon />
+            Editar
+          </MenuItem>
+          <MenuItem
+            onClick={() =>
+              router.push(
+                ROUTES.dashboard.patients.details.info(appointment.patient_id),
+              )
+            }
+          >
+            <ClipboardListIcon />
+            Informações do paciente
+          </MenuItem>
+          <MenuItem
+            onClick={() =>
+              router.push(
+                ROUTES.dashboard.patients.details.history(
+                  appointment.patient_id,
+                ),
+              )
+            }
+          >
+            <ClipboardCheckIcon />
+            Histórico do paciente
+          </MenuItem>
 
-        <Divider className='my-1' />
+          <Divider className='my-1' />
 
-        <MenuItem variant='destructive'>
-          <XCircleIcon />
-          Cancelar
-        </MenuItem>
-      </MenuContent>
-    </Menu>
+          <MenuItem
+            variant='destructive'
+            onClick={() => setCancelModalOpen(true)}
+          >
+            <XCircleIcon />
+            Cancelar
+          </MenuItem>
+        </MenuContent>
+      </Menu>
+
+      {cancelModalOpen && (
+        <Dialog open={cancelModalOpen} onOpenChange={setCancelModalOpen}>
+          <CancelAppointmentModal
+            appointment={appointment}
+            onClose={() => setCancelModalOpen(false)}
+          />
+        </Dialog>
+      )}
+    </>
   )
 }
