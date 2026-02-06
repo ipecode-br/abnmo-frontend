203feab1a597f4c78fa558878a2573b05fb07616
Juliano Sill <sill.juliano@gmail.com>
2026-02-06 15:06:22 -0300

    feat(components): add label to bar chart tooltip

--- src/components/charts/custom-tooltip.tsx
+++ src/components/charts/custom-tooltip.tsx
@@ -4,15 +4,19 @@ export function CustomTooltip({ active, payload, coordinate }: any) {
 
   const { x, y } = coordinate
   const value = payload[0].value
+  const label = payload[0].payload.label
 
   return (
     <div>
       <div
-        className='bg-foreground text-background pointer-events-none absolute flex min-w-8 flex-col items-center rounded-md px-2 py-1.5 text-center text-sm transition-all duration-300 ease-out'
+        className='bg-foreground text-background pointer-events-none absolute flex flex-col items-center gap-2 rounded-md px-3 py-2 text-sm transition-all duration-300 ease-out'
         style={{ left: x, top: y, transform: 'translate(-50%, -155%)' }}
       >
-        {value}
-        <div className='bg-foreground absolute -bottom-0.5 -z-10 size-4 rotate-45' />
+        <div>
+          <span className='font-medium whitespace-nowrap'>{label}:</span>
+          <span className='ml-1 font-bold'>{value}</span>
+        </div>
+        <div className='bg-foreground absolute -bottom-0.5 -z-10 mx-auto size-4 rotate-45' />
       </div>
       <div
         className='bg-primary-soft border-background pointer-events-none absolute size-3 rounded-full border-2 transition-all duration-150 ease-out'
