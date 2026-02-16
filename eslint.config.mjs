9dcbaaa2461b78ad745697acaaceb620eaed53f6
Juliano Sill <sill.juliano@gmail.com>
2026-02-16 10:37:53 -0300

    feat(build): update eslint version and config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,48 +1,33 @@
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-import { FlatCompat } from '@eslint/eslintrc'
-import pluginJs from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
 import eslintPluginPrettier from 'eslint-plugin-prettier'
-import pluginReact from 'eslint-plugin-react'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
-import tseslint from 'typescript-eslint'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  eslintConfigPrettier,
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  ...tseslint.configs.recommended,
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'node_modules/**',
+  ]),
   {
-    ignores: ['.next/**', 'node_modules/**'],
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}', './*.{js,mjs,cjs,ts,jsx,tsx}'],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     plugins: {
-      prettier: eslintPluginPrettier,
       'simple-import-sort': simpleImportSort,
-    },
-    settings: {
-      react: { version: 'detect' },
+      prettier: eslintPluginPrettier,
     },
     rules: {
-      'prettier/prettier': 'error',
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
-      // ignore
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/no-empty-object-type': 'off',
+      'prettier/prettier': 'error',
     },
   },
-]
+])
 
 export default eslintConfig
