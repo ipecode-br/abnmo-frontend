7f248dc18bc56b6559060ef8c2bc56a1a1931d4f
Juliano Sill <sill.juliano@gmail.com>
2026-02-18 23:54:45 -0300

    chore(dashboard): remove access permission to avoid 404 error

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,21 +1,11 @@
-import { redirect } from 'next/navigation'
-
-import { canUser } from '@/actions/auth/can-user'
 import { DashboardHeader } from '@/components/dashboard/header'
 import { DashboardTabButtons } from '@/components/dashboard/tab-buttons'
 import { DashboardWrapper } from '@/components/dashboard/wrapper'
 import { DashboardSidebar } from '@/components/sidebar'
-import { ROUTES } from '@/constants/routes'
 
 export default async function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const canAccess = await canUser('view', 'Dashboard')
-
-  if (!canAccess) {
-    redirect(ROUTES.patient.main)
-  }
-
   return (
     <>
       <DashboardSidebar />
