da23416940a80fcad86fda0d31d5cc5d00353486
Juliano Sill <sill.juliano@gmail.com>
2026-06-01 18:11:44 -0300

    fix: add `**.svm.abnmo.org` to image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from 'next'
 
+import { env } from '@/config/env'
+
 const nextConfig: NextConfig = {
   devIndicators: false,
   // Optimize HMR performance in development
@@ -8,10 +10,20 @@ const nextConfig: NextConfig = {
     pagesBufferLength: 5,
   },
   images: {
-    remotePatterns: [
-      new URL('https://cdn.jsdelivr.net/**'),
-      new URL('https://avatars.githubusercontent.com/**'),
-    ],
+    remotePatterns:
+      env.NODE_ENV === 'production'
+        ? [
+            { protocol: 'https', hostname: '**.svm.abnmo.org' },
+            { protocol: 'https', hostname: 'd2q3o9ng2mw15p.cloudfront.net' },
+            { protocol: 'https', hostname: 'd3esvuqs2o4gq9.cloudfront.net' },
+          ]
+        : [
+            { protocol: 'https', hostname: '**.svm.abnmo.org' },
+            { protocol: 'https', hostname: 'd2q3o9ng2mw15p.cloudfront.net' },
+            { protocol: 'https', hostname: 'd3esvuqs2o4gq9.cloudfront.net' },
+            { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+            { protocol: 'https', hostname: 'cdn.jsdelivr.net' },
+          ],
   },
 }
 
